9f7f525290afd401e370154c33a1d96caefaf27c
merchantakshay <36935205+merchantakshay@users.noreply.github.com>
2018-04-30 18:32:09 -0500

    Add files via upload

---
+++ CreateTablesSQL.sql
@@ -0,0 +1,80 @@
+CREATE TABLE historical_2013 (
+row_id int(5),
+record_date date,
+open_usd float(10,2),
+high_usd float(10,2),
+low_usd float(10,2),
+close_usd float(10,2),
+volume float(15,2),
+market_cap_usd float(15,2),
+coin_id int (20)
+);
+
+CREATE TABLE historical_2014 (
+row_id int(5),
+record_date date,
+open_usd float(10,2),
+high_usd float(10,2),
+low_usd float(10,2),
+close_usd float(10,2),
+volume float(15,2),
+market_cap_usd float(15,2),
+coin_id int (20)
+);
+
+CREATE TABLE historical_2015 (
+row_id int(5),
+record_date date,
+open_usd float(10,2),
+high_usd float(10,2),
+low_usd float(10,2),
+close_usd float(10,2),
+volume float(15,2),
+market_cap_usd float(15,2),
+coin_id int (20)
+);
+
+CREATE TABLE historical_2016 (
+row_id int(5),
+record_date date,
+open_usd float(10,2),
+high_usd float(10,2),
+low_usd float(10,2),
+close_usd float(10,2),
+volume float(15,2),
+market_cap_usd float(15,2),
+coin_id int (20)
+);
+
+CREATE TABLE historical_2017 (
+row_id int(5),
+record_date date,
+open_usd float(10,2),
+high_usd float(10,2),
+low_usd float(10,2),
+close_usd float(10,2),
+volume float(15,2),
+market_cap_usd float(15,2),
+coin_id int (20)
+);
+
+CREATE TABLE historical_2018 (
+row_id int(5),
+record_date date,
+open_usd float(10,2),
+high_usd float(10,2),
+low_usd float(10,2),
+close_usd float(10,2),
+volume float(15,2),
+market_cap_usd float(15,2),
+coin_id int (20)
+);
+
+CREATE TABLE coins (
+id int(5),
+url_coin_name varchar(50),
+coin_name varchar(100),
+symbol varchar(10),
+coin_website varchar(200),
+coin_source_code varchar(200)
+);
